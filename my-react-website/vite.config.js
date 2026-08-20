33632f80cce1ad288497a6605f08f7353ca08cb8
Carson Vitosh <vitoshc@gmail.com>
2026-08-20 00:04:36 -0400

    Fix GitHub Pages deployment

--- my-react-website/vite.config.js
+++ my-react-website/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/Website2026',
+  base: '/Website2026/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -11,3 +11,4 @@ export default defineConfig({
     },
   },
 })
+  
